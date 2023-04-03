17d0dbdf5a9a872464ca5633ca1c6670e8abb135
Sange Sherpa <sherpasange1215@gmail.com>
2023-04-03 09:59:41 +0545

    initial commit

---
+++ app/src/main/java/np/edu/divyagyan/layouts/ConstraintLayoutActivity.java
@@ -0,0 +1,14 @@
+package np.edu.divyagyan.layouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class ConstraintLayoutActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraintlayout);
+    }
+}
